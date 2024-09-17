1053068c8032ad0cfa1f40c8208cf2ae928d152a
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-09-16 22:42:49 -0300

    Buscar errores

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+let cantidad = document.getElementById("cantidad");
+let boton = document.getElementById("generar");
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789";
 
+function generar() {
 
+    let numeroDigitado = parseInt (cantidad.value);
 
+    if (isNan(numeroDigitado) || numeroDigitado < 6) {
+        alert ("La contraseña debe tener un mínimo de 6 caracteres.");
+        return;
+        }
 
+    let contrasenia = "";
+    for(let i=0;i<numeroDigitado;i++) {
+        let caracterAleatorio = Math.floor(Math.random() * cadenaCaracteres.length);
+        contrasenia += cadenaCaracteres[caracterAleatorio];
+        }
+        document.getElementById("contrasena").value = contrasenia;
 
+    }
 
-
-
+boton.onclick = generar;
 
