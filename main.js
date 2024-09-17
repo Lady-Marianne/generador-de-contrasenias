f616f2101582f64225aa5d97cc550f35a1ed4443
Lady-Marianne <mariana.andrea.lois@gmail.com>
2024-09-17 15:12:13 -0300

    Solucionar problema con borrar. Agregar fortaleza contraseña

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let cantidad = document.getElementById("cantidad");
 let boton = document.getElementById("generar");
 let password = document.getElementById("contrasenia");
+let limpiar = document.getElementById("limpiar");
 
-const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789";
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789@#$%&/.-_¡¿?!";
 
 function generar() {
 
@@ -11,15 +12,38 @@ function generar() {
     if (isNaN(numeroDigitado) || numeroDigitado < 6) {
         alert ("La contraseña debe tener un mínimo de 6 caracteres.");
         return;
-        }
+    }
 
     let contrasenia = "";
 
     for(let i=0;i<numeroDigitado;i++) {
         let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
         contrasenia += caracterAleatorio;
-        }
-    
-        password.value = contrasenia;
     }
+    
+    password.value = contrasenia;
+    var mensajeFortaleza = document.getElementById("mensaje-fortaleza");
+
+    if (numeroDigitado >= 6 && numeroDigitado <= 10) {
+        mensajeFortaleza.textContent = "Contraseña débil";
+        setTimeout(function() {mensajeFortaleza.textContent = "";}, 5000);
+    }
+     else if (numeroDigitado > 10 && numeroDigitado <=15) {
+        mensajeFortaleza.textContent = "Contraseña fuerte";
+        setTimeout(function() {mensajeFortaleza.textContent = "";}, 5000);
+     }
+
+    else {
+        mensajeFortaleza.textContent = "Contraseña muy fuerte";
+        setTimeout(function() {mensajeFortaleza.textContent = "";}, 5000);
+    }
+}
+
+function limpiarCampos() {
+    console.log("Función limpiarCampos() llamada");
+    cantidad.value = NaN;
+    password.value = "";
+}
+
+botonLimpiar.addEventListener("click", limpiarCampos);
 
